4da80f947ce4b31a70023879e63f3efeb3e44bee
Кравчук Максим <mak7.kr@gmail.com>
2016-02-26 18:01:29 +0200

    eargae

---
+++ Domain/ContactType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCA.Domain
+{
+    /// <summary>
+    /// Тип контакта (Бот, клиент, потенциальный клиент(лид))
+    /// </summary>
+    public class ContactType : DictionaryEntity
+    {
+    }
+}
---
+++ Domain/Currency.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SCA.Domain
+{
+    /// <summary>
+    /// Справочник валюк
+    /// </summary>
+    public class Currency : DictionaryEntity
+    {
+        [MaxLength(4)]
+        public virtual string BankCode { get; set; }
+    }
+}
