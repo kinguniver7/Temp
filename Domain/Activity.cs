79dac780fb96d665ed71ddec1ba4575158314d01
Кравчук Максим <mak7.kr@gmail.com>
2016-02-26 17:53:59 +0200

    xfgsgdsg

---
+++ Domain/Activity.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SCA.Domain.Interfaces;
+
+namespace SCA.Domain
+{
+    /// <summary>
+    /// Собитие на сайте
+    /// </summary>
+    public class Activity: IdentityEntity, ICreated
+    {
+        /// <summary>
+        /// Тип события
+        /// </summary>
+        public virtual ActivityType Type { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public virtual List<Tag> Tag { get; set; }
+
+        public DateTime CreateDate { get; set; }
+    }
+}
---
+++ Domain/Tag.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCA.Domain
+{
+    /// <summary>
+    /// Класс тега для счетчика
+    /// </summary>
+    public class Tag : NamedEntity
+    {
+        /// <summary>
+        /// Все теги, которые находятся выше по иерархии
+        /// </summary>
+        public virtual List<Tag> ParentTags { get; set; }
+        /// <summary>
+        /// Все теги, которые находятся на том же уровне, что и текущий
+        /// </summary>
+        //public List<Tag> SubsideriesTags { get; set; }  
+    }
+}
