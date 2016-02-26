2ab67c6f6a491a3ad304fa224f19ec5882ab2506
Кравчук Максим <mak7.kr@gmail.com>
2016-02-26 17:27:29 +0200

    Init project

---
+++ Domain/Company.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SCA.Domain.Interfaces;
+
+namespace SCA.Domain
+{
+    /// <summary>
+    /// Класс для компаний, компания может иметь в себе сотруднико
+    /// </summary>
+    public class Company : NamedEntity, IChangedBy, ICreatedBy, ICommented
+    {
+        //Хозяим компании
+        public virtual Contact Owner { get; set; }
+        /// <summary>
+        /// РОдительская компания
+        /// </summary>
+        public virtual Company Parent { get; set; }
+        /// <summary>
+        /// Сотрудники
+        /// </summary>
+        public virtual List<Contact> Employees { get; set; }
+        /// <summary>
+        /// Ответственный исполнитель, человек, который закреплен за компанией
+        /// </summary>
+        //public virtual Employee Executor { get; set; }
+        /// <summary>
+        /// Перечень всех сайтов клиента, на которых установленны счетчики
+        /// </summary>
+        public virtual List<ClientSite> Sites { get; set; }
+
+        public virtual DateTime ModifyDate { get; set; }
+        public virtual SystemUser ChangedBy { get; set; }
+        public virtual DateTime CreateDate { get; set; }
+        public virtual SystemUser CreatedBy { get; set; }
+        public virtual string Comment { get; set; }
+    }
+}
---
+++ Domain/CompanySize.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCA.Domain
+{
+    /// <summary>
+    /// Справочник о размере компании (к-во сотрудников)
+    /// </summary>
+    public class CompanySize: DictionaryEntity
+    {
+    }
+}
